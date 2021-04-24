5a9fd60058e57c45f6c04e3478780d5f69a10526
Muneer Alam <77039639+Muneer320@users.noreply.github.com>
2021-04-24 16:09:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas, database;
+var gameState = 0;
+var playerCount;
+var form, game, player;
+
+
+function setup() {
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500, 500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  if (playerCount === 4) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    clear();
+    game.play();
+  }
+}
